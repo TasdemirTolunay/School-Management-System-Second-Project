8ca6aa930c80b440dad1845639fda1c8f5f12414
Tolunay <tasdemirtolunay@gmail.com>
2021-08-18 17:19:57 +0300

    Update StudentService and StudentController

--- school-management/src/main/java/dev/patika/schoolmanagement/controller/StudentController.java
+++ school-management/src/main/java/dev/patika/schoolmanagement/controller/StudentController.java
@@ -1,19 +1,18 @@
 package dev.patika.schoolmanagement.controller;
 
 
+import dev.patika.schoolmanagement.model.Address;
+import dev.patika.schoolmanagement.model.Course;
 import dev.patika.schoolmanagement.model.Student;
 import dev.patika.schoolmanagement.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/student")
 public class StudentController {
 
     StudentService studentService;
@@ -23,10 +22,59 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @GetMapping("/students")
+    @GetMapping("/list")
     public ResponseEntity<List<Student>> findAllStudent(){
 
-        return new ResponseEntity<>(studentService.findAllStudent(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(studentService.findAllStudent(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> findStudentById(@PathVariable int id){
+
+        return new ResponseEntity<>(studentService.findStudentById(id), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/courses/{id}")
+    public ResponseEntity<List<Course>> findCoursesOfStudent(@PathVariable int id){
+
+        return new ResponseEntity<>(studentService.findCourseOfStudent(id), HttpStatus.ACCEPTED);
+
+    }
+
+    @GetMapping("/address/{id}")
+    public ResponseEntity<Address> findAddressOfStudent(@PathVariable int id){
+
+        return new ResponseEntity<>(studentService.findAddressOfStudent(id),HttpStatus.ACCEPTED);
+
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.OK);
+
+    }
+
+    @PutMapping("/update/{id}")
+    public void updateToStudent(@RequestBody Student student, @PathVariable int id){
+
+        studentService.updateStudent(student,id);
+
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteStudentOfObject(@RequestBody Student student){
+
+        studentService.deleteStudent(student);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudentById(@PathVariable int id){
+
+        studentService.deleteStudentById(id);
 
     }
 
--- school-management/src/main/java/dev/patika/schoolmanagement/service/StudentService.java
+++ school-management/src/main/java/dev/patika/schoolmanagement/service/StudentService.java
@@ -5,12 +5,10 @@ import dev.patika.schoolmanagement.model.Course;
 import dev.patika.schoolmanagement.model.Student;
 import dev.patika.schoolmanagement.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -19,8 +17,7 @@ public class StudentService {
     StudentRepository studentRepository;
 
     @Autowired
-    public StudentService(@Qualifier("studentRepo") StudentRepository studentRepository) {
-
+    public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
@@ -30,5 +27,50 @@ public class StudentService {
 
     }
 
+    public Student findStudentById(int id){
+
+        return  studentRepository.findById(id).get();
+
+    }
+
+    public Student saveStudent(Student student){
+
+        return studentRepository.save(student);
+
+    }
+
+    public void deleteStudent(Student student){
+
+        studentRepository.delete(student);
+
+    }
+
+    public void deleteStudentById(int id){
+
+        studentRepository.deleteById(id);
+
+    }
+
+    public void updateStudent(Student student, int id){
+
+        Student findStudent = findStudentById(id);
+        findStudent.setStudentName(student.getStudentName());
+        findStudent.setStudentGender(student.getStudentGender());
+        findStudent.setStudentBirthDate(student.getStudentBirthDate());
+        studentRepository.save(findStudent);
+
+    }
+
+    public List<Course> findCourseOfStudent(int id){
+
+        return findStudentById(id).getCourses();
+
+    }
+
+    public Address findAddressOfStudent(int id){
+
+        return findStudentById(id).getAddress();
+
+    }
 
 }
